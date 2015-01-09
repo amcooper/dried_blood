c9a9d44423c15d039bca023d957b416bc1bd2f7b
Marc <you@example.com>
2015-01-09 14:57:35 -0500

    baddies hash

---
+++ rpg.rb
@@ -0,0 +1,15 @@
+puts "Enter Your Name, Warrior:"
+  name = gets.chomp
+  puts
+
+baddies = [
+  {name => "Newt", health => 10},
+  {name => "George", health => 10},
+  {name => "Reagan", health => 10},
+  {name => "Bush", health => 10},
+  {name => "Gore", health => 10},
+  {name => "Koch", health => 10},
+  {name => "Anderson" ,health => 10},
+  {name => "Palin",health => 10},
+  {name => "Nixon",health => 10},
+  ]
