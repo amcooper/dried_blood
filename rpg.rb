19901d8bf4918af1489cb7c9a4515a7cb00db5e4
Ciara Foran <Ciara@Ciaras-MacBook-Air.local>
2015-01-09 14:55:04 -0500

    first commit with baddie attack

---
+++ rpg.rb
@@ -0,0 +1,22 @@
+def baddie_attack(opponent) do
+  chance = rand
+  if chance > .4
+    points = rand(1..3)
+    puts "The #{opponent[name]} attacks you for #{opponent[name]} points."
+    hero[health] = hero[health] - points
+    puts hero[name] + ": " + hero[health] + " health points."
+    puts opponent[name] + ": " + opponent[health] + " health points."
+    if hero[health] <= 0
+      hero_death
+    end
+  else
+    puts "The #{opponent[name]} tried to attack you but misses!"
+    puts hero[name] + ": " + hero[health] + " health points."
+    puts opponent[name] + ": " + opponent[health] + " health points."
+  end
+end
+
+def hero_death do
+  puts "Sorry the blood has run dry. Better luck next time!"
+  break
+end
